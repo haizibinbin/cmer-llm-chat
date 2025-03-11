0cabe4ddc5d8fc6396aabb7eb16978352b3fa8e4
hbb <haizibinbin@gmail.com>
2025-03-11 17:17:56 +0800

    fix

--- src/models/BaseModel.php
+++ src/models/BaseModel.php
@@ -11,12 +11,10 @@ class BaseModel
      */
     public function toArray() {
         $vars = get_object_vars($this);
+        // 如果 $value 是空数组，或空字符串，则过滤掉
         return array_filter($vars, function($value) {
-            // 如果 $value 是空数组，或空字符串，则过滤掉
-            if ((is_array($value) || is_string($value)) && empty($value)) {
-                return false;
-            }
-            return $value;
+            // 只有当回调函数返回 true 时，元素才会保留在数组中。
+            return !((is_array($value) || is_string($value)) && empty($value));
         });
     }
 }
